9ad05363b555ea7300d1c532c1301f97eb6651c2
Irina Barskaya <drozdyuk.irina@gmail.com>
2017-08-28 16:13:52 -0500

    Added SPGR method

--- functions/SPGR_rp.m
+++ functions/SPGR_rp.m
@@ -1,4 +1,5 @@
-function Mxyf = SPGR_rp(parameters, Wm, offsets, omega1, R1f_obs, tau)
+function Mxyf = SPGR_rp(parameters, Wm, offsets, omega1, MT_angles, ...
+                        R1f_obs, tau, TR, flip_angle)
 %{
 function Mxyf = SPGR_rp(parameters, Wr, offsets, omega1, R1f_obs) 
          calculates Mxy value using Sled and Pike rectangular pulse model 
@@ -16,9 +17,12 @@ input:
     Wm - averaged saturation rate of macromolecular pool, [s-1]
     omega1 - array of averaged omega1 value, []
     offsets - array of frequency offsets for MT pulse, [Hz]
+    MT_angles - array of flip angles of MT pulse, [rad]
     R1f_obs - measured from separate experiment R1 relaxation of free pool,
               [s-1]
     tau - full-width at half-maximum value of MT shaped pulse, [s-1]
+    TR - repetition time, [s-1]
+    flip_angle - excitation flip angle, [rad]                   
 output:
     Mxyf - normalized(!) proton magnetization vector
 %}
@@ -36,14 +40,11 @@ output:
 
     R1f = R1f_obs - kfm *(R1m - R1f_obs) / (R1m - R1f_obs + kfm/PSR);
 
-    TR = pulse.TR;
-    flip_angles = pulse.flip_angle /180 * pi;   % MT pulse flip angles, [rad]
-
 % saturation of the free pool due to the MT pulse. 
 % Kevin, you  speed up fitting by make Sf pre-computed, because it only
 % depends on shape of pulse, its amplitude, power and offset. So you can 
 % create table Sf(offset, power).
-    Sf = compute_Sf(flip_angles,offsets,T2f);
+    Sf = compute_Sf(MT_angles,offsets,T2f);
     
  %{
     Kevin, according to rp approximation pulse sequence has the following 
@@ -61,10 +62,8 @@ output:
   %}
     
     % now have to iterate across different powers (compared to Sled&Pike)
-    num_powers = length(omega1);
     M0 = [1; PSR];
     Sr = 1; % alternatively can be changed
-    S = [Sf; Sr];
     
     % loop over different powers of saturation pulse and offsets. Maybe
     % flattening W to an array vs keeping it as a matrix to get rid of one inner
@@ -75,13 +74,13 @@ output:
     Mxy = zeros(num_offsets, num_powers);
     for i=1:num_powers
         for j=1:num_offsets
-            Mxy(j, i)  = compute_Mxy_rp(kmf, R1m, kfm, R1f, S,  Wm(j,i), ...
-                                        TR,flip_angles(i), tau, M0);                              
+            S = [Sf(j,i); Sr];
+            Mxy(j, i)  = compute_Mxy_rp(kmf, R1m, kfm, R1f, S,  Wm(j,i), TR, flip_angle, tau, M0);  
         end
     end
     % no MT pulse (for normalization) Sf = 1; Wm = 0; Sr = 1;
     Mxy0  = compute_Mxy_rp(kmf, R1m, kfm, R1f, [1; 1], 0, TR, ...
-                           flip_angles, tau, M0);
+                           flip_angle, tau, M0);
                              
     Mxyf = Mxy ./ Mxy0; % normalized value
 
--- functions/compute_Mxy_rp.m
+++ functions/compute_Mxy_rp.m
@@ -1,4 +1,5 @@
-function Mxyf = compute_Mxy_rp(kmf, R1m, kfm, R1f, S, Wm, TR, flip_angle, tau, M0)
+function Mxyf = compute_Mxy_rp(kmf, R1m, kfm, R1f, S, Wm, TR, ...
+                               flip_angle, tau, M0)
 %{
 function Mxyf = compute_Mxy_rp(kmf, R1m, kfm, R1f, Sf, Wr, TR, flip_angles)
          computes Mxy value using signal equation based on Sled and Pike 
@@ -14,7 +15,7 @@ input:
     Sf - correction for saturation effect of MT pulse on free pool
     Wm - averaged saturation rate of macromolecular pool, [s-1]
     TR - repetition time of pulse sequence, [s]
-    flip_angle - flip angle of MT pulse, [rad]
+    flip_angle - excitation flip angle, [rad]
     M0 = [1; PSR] 
     tau - full-width at half-maximum value of MT shaped pulse, [s-1]
 
@@ -24,6 +25,8 @@ output:
 
     M0f = M0(1);
     M0m = M0(2);
+    Sf = S(1);
+    Sm = S(2);
     
     % free precession
     Afp  = [R1f+kfm,  -kmf; -kfm,  R1m+kmf];
@@ -42,11 +45,28 @@ output:
     Mzm_ss = numerator_m/ denominator; 
     
     Mz_ss = [Mzf_ss; Mzm_ss];
+    S = diag([Sf.*cos(flip_angle) Sm]);
     I = eye(2);
-
-%     Mz = (I - expAcw * expAfp * expAcw * diag([Sf.*cos(alpha) Sr]))\ ...
-%         ( (I + expAcw * (-I + expAfp * (I -  expAcw)))*Mss + expAcw*(I - expAfp)*M0_inf);
     
-    Mxyf = Mz(1,:).*sin(flip_angle).*S(:,1);
+    % solving equation for steady state Mz(t) = Mz(t+TR)
+    % in case of rectangular approximation:
+ %{
+   * instantaneous saturation of the free pool from the MT and excitation
+     pulse:
+     Mis = S*M
+   * continuous-wave irradiation of the restricted  pool for a period tau/2:
+     Mcw1 = expAcw * Mis + (I - expAcw) * Mss
+   * period TR - tau of free precession
+     Mfp = expAfp * Mcw1 + (I - expAf) * M0
+   * period of continuous-wave irradiation of duration tau/2
+     Mcw2 = expAcw * Mfp + (I - expAcw) * Mss
+    
+    %}
+   
+     Mz = (I - expAcw * expAfp * expAcw * S)\ ...
+          (expAcw * (I - expAfp) * M0 + (I - ...
+                             expAcw * (1 - expAfp * (1 - expAcw)))) * Mz_ss;       
+
+     Mxyf = Mz(1,:).*sin(flip_angle).*Sf;
 
 end
